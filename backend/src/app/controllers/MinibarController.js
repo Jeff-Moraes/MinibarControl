21568b9a7481248bb2b846a3c50322cdba647226
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-02-02 16:33:48 +0100

    Minibar index/show permission

--- backend/src/app/controllers/MinibarController.js
+++ backend/src/app/controllers/MinibarController.js
@@ -2,12 +2,20 @@ import Minibar from '../models/Minibar';
 
 class MinibarController {
   async index(req, res) {
+    if (!req.userAdmin) {
+      return res.status(400).json({ error: 'User is not Admin' });
+    }
+
     const allMinibars = await Minibar.findAll();
 
     return res.json(allMinibars);
   }
 
   async show(req, res) {
+    if (!req.userAdmin) {
+      return res.status(400).json({ error: 'User is not Admin' });
+    }
+
     const { minibarId } = req.params;
 
     const minibar = await Minibar.findByPk(minibarId);
